e2729ea780da01123ea6de858f776a1e1dfa6e50
shanedunne <shanejjdunne@hotmail.com>
2021-04-16 21:00:46 +0100

    added search bar and styled

--- assets/js/coin-p-1.js
+++ assets/js/coin-p-1.js
@@ -44,7 +44,7 @@ async function getData(){
     // Write your code here
     function buildTable() {
         let coinData = `
-        <table>
+        <table id='myTable'>
           <thead>
             <tr>
               <th class='rank-head'>Rank</th>
@@ -116,4 +116,24 @@ async function getData(){
 
 getData();
 
-console.log($(this));
+// Search function - https://www.w3schools.com/howto/howto_js_filter_table.asp 
+function searchFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search-bar");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
